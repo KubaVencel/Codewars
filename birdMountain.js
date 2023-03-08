062f88625a70a333823923f1b77be1eefc472a65
Jakub Vencel <j.vencel27@gmail.com>
2023-03-08 18:29:53 +0100

    adeed Bird Mountait codewars kata solution

---
+++ birdMountain.js
@@ -0,0 +1,67 @@
+/*
+
+Kata Task
+
+A bird flying high above a mountain range is able to estimate the height of the highest peak.
+
+Can you?
+Example
+The birds-eye view
+
+^^^^^^
+ ^^^^^^^^
+  ^^^^^^^
+  ^^^^^
+  ^^^^^^^^^^^
+  ^^^^^^
+  ^^^^
+
+The bird-brain calculations
+
+111111
+ 1^^^^111
+  1^^^^11
+  1^^^1
+  1^^^^111111
+  1^^^11
+  1111
+
+111111
+ 12222111
+  12^^211
+  12^21
+  12^^2111111
+  122211
+  1111
+
+111111
+ 12222111
+  1233211
+  12321
+  12332111111
+  122211
+  1111
+
+Height = 3
+Series
+
+    Bird Mountain
+    Bird Mountain - the river
+
+*/
+
+var peakHeight = function(mountain, counter = 0) {
+  if (mountain.every(row => row.every(sq => sq === " "))) return counter;
+
+  let afterEdgeCheck = mountain.map((row, i) => row.map((sq, j) => {
+    let up = mountain[i - 1] ? mountain[i - 1][j] : " ";
+    let down = mountain[i + 1] ? mountain[i + 1][j] : " ";
+    let left = mountain[i][j - 1] || " ";
+    let right = mountain[i][j + 1] || " ";
+    return [up, down, left, right].includes(" ") ? " " : sq;
+  }));
+
+  return peakHeight(afterEdgeCheck, ++counter);
+}
+
+
