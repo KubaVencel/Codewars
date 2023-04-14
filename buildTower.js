7bb5b10559956dd86863af87bd510e69e6cde9bd
Jakub Vencel <j.vencel27@gmail.com>
2023-04-14 22:18:50 +0200

    added build tower codewars kata solution

---
+++ buildTower.js
@@ -0,0 +1,38 @@
+/*
+
+Build Tower
+
+Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors. A tower block is represented with "*" character.
+
+For example, a tower with 3 floors looks like this:
+
+[
+  "  *  ",
+  " *** ", 
+  "*****"
+]
+
+And a tower with 6 floors looks like this:
+
+[
+  "     *     ", 
+  "    ***    ", 
+  "   *****   ", 
+  "  *******  ", 
+  " ********* ", 
+  "***********"
+]
+
+Go challenge Build Tower Advanced once you have finished this :)
+
+*/
+
+function towerBuilder(nFloors) {
+  const result = [];
+  for (let floor = 1; floor <= nFloors; floor++) {
+    const spaces = ' '.repeat(nFloors - floor);
+    const asterisk = '*'.repeat(floor * 2 - 1);
+    result.push(`${spaces}${asterisk}${spaces}`)
+  }
+  return result;
+}
