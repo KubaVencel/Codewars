ac3a2bfe15173f29527e3f14299dfdd87c61c1c0
Jakub Vencel <j.vencel27@gmail.com>
2023-06-07 21:18:28 +0200

    added solution to brak cammel case codewars kata

---
+++ breakCamelCase.js
@@ -0,0 +1,16 @@
+/*
+
+Complete the solution so that the function will break up camel casing, using a space between words.
+Example
+
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""
+
+*/
+
+function solution(string) {
+  string = string.split(/(?=[A-Z])/).join(" ")
+  return string
+}
+
