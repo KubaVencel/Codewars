620dc19188995d67d6b2fd7fc0ead3574d776259
Jakub Vencel <j.vencel27@gmail.com>
2023-06-15 22:07:15 +0200

    added solution to Street Fighter 2 Character Selection codewars kata

--- validBraces.js
+++ validBraces.js
@@ -27,7 +27,6 @@ function validBraces(braces) {
 
   for (var i = 0; i < braces.length; i++) {
     currentChar = braces[i];
-
     if (matches[currentChar]) { // opening braces
       stack.push(currentChar);
     } else { // closing braces
@@ -36,6 +35,5 @@ function validBraces(braces) {
       }
     }
   }
-
   return stack.length === 0; // any unclosed braces left?
 }
