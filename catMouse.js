cf87bd4783184c700a8d41490fa86eaade5b5a30
Jakub Vencel <j.vencel27@gmail.com>
2023-11-28 18:37:26 +0100

    added solution to Cat and Mouse - 2D Version codewars kata

---
+++ catMouse.js
@@ -0,0 +1,42 @@
+/*
+ 
+Description:
+
+You will be given a string (map) featuring a cat "C" and a mouse "m". The rest of the string will be made up of dots (".") The cat can move the given number of moves up, down, left or right, but not diagonally.
+
+You need to find out if the cat can catch the mouse from it's current position and return "Caught!" or "Escaped!" respectively.
+
+Finally, if one of two animals are not present, return "boring without two animals".
+Examples
+
+moves = 5
+
+map =
+..C......
+.........
+....m....
+
+returns "Caught!" because the cat can catch the mouse in 4 moves
+
+moves = 5
+
+map =
+.C.......
+.........
+......m..
+
+returns "Escaped!" because the cat cannot catch the mouse in  5 moves
+
+*/
+
+function catMouse(map, moves) {
+  const indexOfC = map.indexOf('C')
+  const indexOfm = map.indexOf('m')
+  if (!~indexOfC || !~indexOfm) {
+    return 'boring without two animals'
+  }
+  const stepLength = map.indexOf(`
+`) + 1
+  const step = Math.abs(indexOfC % stepLength - indexOfm % stepLength) + Math.abs(parseInt(indexOfC / stepLength) - parseInt(indexOfm / stepLength))
+  return step > moves ? 'Escaped!' : 'Caught!'
+}
