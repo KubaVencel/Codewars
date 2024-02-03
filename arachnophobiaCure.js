9519a3457c2de2b212c87f8fda377b5aa3f78e82
Jakub Vencel <j.vencel27@gmail.com>
2024-02-03 20:50:33 +0100

    added solution to Curing Arachnophobia codewars kata

---
+++ arachnophobiaCure.js
@@ -0,0 +1,36 @@
+/*
+
+DESCRIPTION:
+There is no single treatment that works for every phobia, but some people cure it by being gradually exposed to the phobic situation or object. In this kata we will try curing arachnophobia by drawing primitive spiders.
+
+Our spiders will have legs, body, eyes and a mouth. Here are some examples:
+
+/\((OOwOO))/\
+
+/╲(((0000w0000)))╱\
+
+^(oWo)^
+You will be given four values:
+
+leg size where each value stands for its own leg type: 1 for "^ ^", 2 for "/\ /\", 3 for "/╲ ╱\", 4 for "╱╲ ╱╲"
+body size where each value stands for its own body type: 1 for "( )", 2 for "(( ))", 3 for "((( )))"
+mouth representing the spider's mouth
+eye representing the spider's eye
+Note: the eyes are symmetric, and their total amount is 2 to the power of body size.
+
+You will also be given only valid data. That's it for the instructions, you can start coding!
+
+*/
+
+function drawSpider(legSize, bodySize, mouth, eye) {
+  console.log(legSize, bodySize, mouth, eye);
+  let legs = legSize === 1 ? "^" :
+    legSize === 2 ? "/\\" :
+      legSize === 4 ? "╱╲" : "";
+  let bodyLeft = Array(bodySize).fill("(").join("");
+  let bodyRight = Array(bodySize).fill(")").join("");
+  let eyes = Array(Math.pow(2, bodySize - 1)).fill(eye).join("");
+  return (legSize == 3 ? "/╲" : legs) +
+    bodyLeft + eyes + mouth + eyes + bodyRight +
+    (legSize == 3 ? "╱\\" : legs);
+}
