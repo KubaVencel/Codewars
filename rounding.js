a875b12a877340f2a7300e5a0a9a8d2f5020a445
Jakub Vencel <j.vencel27@gmail.com>
2024-03-21 17:48:45 +0100

    added solution for Simple Fun #181: Rounding codewars kata

---
+++ rounding.js
@@ -0,0 +1,35 @@
+/*
+
+Task
+
+Round the given number n to the nearest multiple of m.
+
+If n is exactly in the middle of 2 multiples of m, return n instead.
+Example
+
+For n = 20, m = 3, the output should be 21.
+
+For n = 19, m = 3, the output should be 18.
+
+For n = 50, m = 100, the output should be 50.
+Input/Output
+
+    [input] integer n
+
+1 ≤ n < 10^9.
+
+    [input] integer m
+
+3 ≤ m < 109.
+
+    [output] an integer
+
+*/
+
+function rounding(n, m) {
+  const under = Math.floor(n / m) * m
+  const above = under + m
+  if (n - under < above - n) return under
+  if (n - under > above - n) return above
+  return n
+}
